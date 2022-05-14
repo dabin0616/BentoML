f363b8d063d595fdaf5b313857481265008e0610
DaBin Lee <leedabin0616@gmail.com>
2022-05-14 20:24:14 +0900

    koelectra finetuning start

--- KcElectra.py
+++ KcElectra.py
@@ -1,25 +1,52 @@
 import argparse
-import pickle
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from pathlib import Path
-from torch.utils.data import DataLoader
-from transformers.modeling_bert import BertConfig
-from pretrained.tokenization_ranked import FullTokenizer as KBertRankedTokenizer
-from transformers import BertTokenizer
-from model.net import SentenceClassifier
-from model.data import Corpus
-from model.utils import PreProcessor, PadSequence
-from model.metric import evaluate, acc
-from utils import Config, CheckpointManager, SummaryManager
-from tqdm import tqdm
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from sklearn.preprocessing import LabelEncoder
 from torch.utils.tensorboard import SummaryWriter
 
 
-def main():
+def dataloader(args, step):
+
+    if step == 'train':
+
+        train_df = pd.read_csv('%d/%d'%(args.data_dir, args.train_data), sep='\t')
+        train_sample = RandomSampler(train_df)
+        train, dev = train_test_split()
+    elif step == 'test':
+        test_df = pd.read_csv('%d/%d' % (args.data_dir, args.test_dat), sep='\t')
+        test_sample = RandomSampler(test_df)
+
+def train():
+
+
+
+def test(args):
+
+
+
+def main(parser):
+
+    args = parser.parse_args()
 
 
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', default='NLI', help="수행하는 task 이름")
+    parser.add_argument('--data_dir', default='data/KorNLI', help="데이터의 디렉토리")
+    parser.add_argument('--train_data', default='multinli.train.ko.tsv', help="사용하는 파일 이름")
+    parser.add_argument('--test_data', default='multinli.train.ko.tsv', help="사용하는 파일 이름")
+    parser.add_argument('--batch', default=5, help="batch 크기")
+    parser.add_argument('--epoch', default=1, help="학습을 진행하는 횟수")
+    parser.add_argument('--max_len', default=300, help="입력의 문장 길이")
+    parser.add_argument('--dropout', default=0.1, help="dropout 비율")
+    parser.add_argument('--warmup_ratio', default='', help="")
+    parser.add_argument('--max_gram_norm', default='', help="")
+    parser.add_argument('--lr', default='', help="")
+    parser.add_argument('--log_interval', default='', help="")
+    parser.add_argument('--', default='', help="")
+
+
+    main(parser)
